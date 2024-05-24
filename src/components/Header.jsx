3501f3e7f9ee9ab12e1a2b2508bb95f48c530fd8
sentobc13 <sentoberlanga@gmail.com>
2024-05-24 14:06:09 +0200

    links header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='menu'>
+     <Link to='/'>Home</Link>
+     <Link to='/form'>Form</Link>
+     <Link to='/list'>List</Link>
+    </nav>
+  )
+}
+
+export default Header
