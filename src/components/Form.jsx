06eaab21b467f91da7ea19dd917a0dcc8a7ab303
sentobc13 <sentoberlanga@gmail.com>
2024-05-26 12:42:08 +0200

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+    const initialValues = {
+        name: "",
+        title: "",
+        author: "",
+        description: ""
+
+    }
+
+    const clearValues = () => {
+        setData(initialValues)
+    }
+    const [data,setData] = useState(initialValues)
+
+
+    const handleInputChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        localStorage.setItem('reserva', JSON.stringify(data))
+        clearValues();
+        setTimeout(() => {
+            navigate("/");
+          }, 1000)
+    }
+
+    
+    return <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+            onChange={handleInputChange}
+            value={data.name}
+        />
+        <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Title"
+            onChange={handleInputChange}
+            value={data.name} />
+
+        <input
+            type="text"
+            name="author"
+            id="author"
+            placeholder="Author"
+            onChange={handleInputChange}
+            value={data.name} />
+
+        <input
+            type="text"
+            name="Author"
+            id="description"
+            placeholder="Description"
+            onChange={handleInputChange}
+            value={data.name}
+        />
+
+        <button type="submit">Send</button>
+    </form>
 }
 
 export default Form
