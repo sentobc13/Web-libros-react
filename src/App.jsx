46fbb020845d7194de1f79ebc9f774d32b53ce30
sentobc13 <sentoberlanga@gmail.com>
2024-05-26 13:01:10 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import ListNews from './components/ListNews/ListNews'
 import Form from './components/Form/Form'
+import Footer from './components/Footer/Footer'
 
 
 const API_URL = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=ODg6YIGnkG7BRGGEg4VEKacGnDIQPAMH'
@@ -20,6 +21,7 @@ function App() {
           <Route path='/form' element={<Form/>}/>
           <Route path='/list' element={<ListNews/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    <p>&copy; 2024 Sento Berlanga. Todos los derechos reservados.</p>
+    <p>
+      <a href="https://www.linkedin.com/in/tu-perfil" target="_blank">LinkedIn</a> |
+      <a href="https://github.com/tu-usuario" target="_blank">GitHub</a> |
+      <a href="mailto:tu-email@example.com">Email</a>
+    </p>
+  </footer>
+  
   )
 }
 
